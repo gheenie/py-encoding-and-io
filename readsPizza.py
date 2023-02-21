0123576eb24ee9b8dc4c23854457ff0c25103472
infoomics <sbx_94@hotmail.com>
2023-02-21 15:07:44 +0000

    complete extension tasks

--- readsPizza.py
+++ readsPizza.py
@@ -1,31 +1,32 @@
 import csv
 from operator import itemgetter
+import json
 
 pizzas = []
 with open('data/pizza.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file, delimiter=',')
     for row in reader:
         pizzas.append(row)
-print(pizzas)
+#print(pizzas)
 
 capri_pizza = [pizza['Description'] for pizza in pizzas if pizza['Pizza'] == 'Capri']
 
 ingredients = capri_pizza[0]
 ingredients = ingredients.split(', ')
 for ingredient in ingredients:
-    print(ingredient)
+    #print(ingredient)
+    pass
 
 under_nine = [pizza for pizza in pizzas if float(pizza['Cost'][1:]) < 9]
 under_nine_by_cost = sorted(under_nine, key = itemgetter('Cost'))
 
-print(under_nine_by_cost) 
+#print(under_nine_by_cost) 
 
 new_pizza = 'BBQ Chicken,£10.70,"Cheese, tomato, BBQ sauce, onions and chicken",950kcal\n'
 
 # with open('data/pizza.csv', 'a', encoding='utf-8') as file:
 #     file.write(new_pizza)
 
-increase_cost = []
 for pizza in pizzas:
     updated_cost = float(pizza['Cost'][1:])
     updated_cost *= 1.1
@@ -35,3 +36,7 @@ for pizza in pizzas:
     pizza['Cost'] = updated_cost
     print(pizza)
 
+json_pizzas = { 'pizzas': pizzas }
+
+with open('data/pizzas.json', 'w', encoding='utf-8') as file:
+    json.dump(json_pizzas, file)
